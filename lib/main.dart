ce93d53f9a9d86ae5f20673ae847a8117fa5fd9d
Suelio Pss <rav@Ravs-Mac-mini.local>
2020-09-17 19:52:24 -0300

    Load local json file

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:components/src/pages/home_temp.dart';
+import 'package:components/src/pages/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Components APP',
-      home: HomePageTemp(),
+      home: HomePage(),
     );
   }
 }
---
+++ lib/src/pages/home_page.dart
@@ -0,0 +1,19 @@
+import 'package:components/src/providers/menu_provider.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Components'),
+      ),
+      body: _list(),
+    );
+  }
+
+  Widget _list() {
+    menuProvider.loadData();
+    return ListView();
+  }
+}
