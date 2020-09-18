791fb6fd9b691ff73265d2590cbc92a98d3a5c50
Suelio Pss <rav@Ravs-Mac-mini.local>
2020-09-18 16:35:27 -0300

    Animated Container

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -6,6 +6,31 @@ class AvatarPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Avatar Page'),
+        actions: <Widget>[
+          Container(
+            padding: EdgeInsets.all(7.0),
+            child: CircleAvatar(
+              radius: 22.0,
+              backgroundImage: NetworkImage(
+                  "https://conteudo.imguol.com.br/blogs/88/files/2018/11/monkeymasterstanlee-1-e1542048422172_rthu.jpg"),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(right: 12.0),
+            child: CircleAvatar(
+              child: Text('SP'),
+              backgroundColor: Colors.brown,
+            ),
+          )
+        ],
+      ),
+      body: Center(
+        child: FadeInImage(
+          placeholder: AssetImage('assets/jar-loading.gif'),
+          image: NetworkImage(
+              "https://veja.abril.com.br/wp-content/uploads/2019/11/stan-lee.jpg"),
+          fadeInDuration: Duration(milliseconds: 200),
+        ),
       ),
     );
   }
