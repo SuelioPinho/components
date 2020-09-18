55ce7a9f13bdddf6ee2bc8432143df06d69929fa
Suelio Pss <rav@Ravs-Mac-mini.local>
2020-09-18 13:52:11 -0300

    Create custom cards

--- lib/src/pages/card_page.dart
+++ lib/src/pages/card_page.dart
@@ -7,6 +7,71 @@ class CardPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Cards'),
       ),
+      body: ListView(
+        padding: EdgeInsets.all(10.0),
+        children: <Widget>[
+          _card1(),
+          SizedBox(
+            height: 30.0,
+          ),
+          _card2()
+        ],
+      ),
+    );
+  }
+
+  Card _card1() {
+    return Card(
+        elevation: 8.0,
+        shape:
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+        child: Column(
+          children: [
+            ListTile(
+              title: Text('titulo'),
+              subtitle: Text('Subtitle'),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                FlatButton(onPressed: null, child: Text('Cancel')),
+                FlatButton(onPressed: null, child: Text('Ok'))
+              ],
+            )
+          ],
+        ));
+  }
+
+  Container _card2() {
+    final card = Container(
+        child: Column(
+      children: <Widget>[
+        FadeInImage(
+            height: 300,
+            fit: BoxFit.cover,
+            placeholder: AssetImage('assets/jar-loading.gif'),
+            fadeInDuration: Duration(milliseconds: 200),
+            image: NetworkImage(
+                "https://www.tom-archer.com/wp-content/uploads/2018/06/milford-sound-night-fine-art-photography-new-zealand.jpg")),
+        Container(padding: EdgeInsets.all(10.0), child: Text('Figura 1'))
+      ],
+    ));
+
+    return Container(
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20.0),
+          color: Colors.white,
+          boxShadow: <BoxShadow>[
+            BoxShadow(
+                color: Colors.black26,
+                blurRadius: 10.0,
+                spreadRadius: 2.0,
+                offset: Offset(2.0, 10.0))
+          ]),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(20.0),
+        child: card,
+      ),
     );
   }
 }
